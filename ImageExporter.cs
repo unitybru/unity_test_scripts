f034c570950f8eedf6093d07f2c3d498033acbc4
Bruno Angeles <bruno.angeles@unity3d.com>
2022-05-25 16:09:17 -0400

    Implementation just like documentation at https://docs.unity3d.com/2022.1/Documentation/ScriptReference/ScreenCapture.CaptureScreenshotIntoRenderTexture.html

--- ImageExporter.cs
+++ ImageExporter.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.ComponentModel;
 using System.IO;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 /// <summary>
 /// This behaviour exports images of your project to disk when a key is pressed.
@@ -12,14 +13,19 @@ public class ImageExporter : MonoBehaviour
     {
         Screen,
         GameView,
-        GameViewRT,
+        GameViewRenderTexture,
     };
 
     public KeyCode recordKey = KeyCode.Space;
-    public ImageExportMethod method = ImageExportMethod.GameViewRT;
+    public ImageExportMethod method = ImageExportMethod.GameViewRenderTexture;
     private RenderTexture captureTexture;
 
     void Start()
+    {
+        CreateRenderTexture();
+    }
+
+    void CreateRenderTexture()
     {
         captureTexture = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGB32)
         {
@@ -28,6 +34,7 @@ public class ImageExporter : MonoBehaviour
         captureTexture.Create();
         captureTexture.name = "ImageExporter_captureTexture";
     }
+
     // Update is called once per frame
     void Update()
     {
@@ -37,40 +44,60 @@ public class ImageExporter : MonoBehaviour
         }
     }
 
+    private FileInfo GetCurrentFileName()
+    {
+        var paddedFrameString = Time.frameCount.ToString().PadLeft(8, '0');
+        var currentFile = new FileInfo($"test_{paddedFrameString}.png");
+        return currentFile;
+    }
+
+    private void OutputTextureToDisk(Texture2D tex2d)
+    {
+        var currentFile = GetCurrentFileName();
+        var imgBytes = tex2d.EncodeToPNG();
+        File.WriteAllBytes(currentFile.FullName, imgBytes);
+        Debug.Log($"Wrote frame #{Time.frameCount} to disk as {currentFile.FullName}");
+    }
+
     IEnumerator RecordAFrame()
     {
         // Wait until after the frame has been prepared
         yield return new WaitForEndOfFrame();
-
-        var paddedFrameString = Time.frameCount.ToString().PadLeft(8, '0');
-        var currentFile = new FileInfo($"test_{paddedFrameString}.png");
 
         Texture2D tex2d = null;
         switch (method)
         {
             case ImageExportMethod.GameView:
                 tex2d = ScreenCapture.CaptureScreenshotAsTexture();
+                OutputTextureToDisk(tex2d);
                 break;
             case ImageExportMethod.Screen:
                 tex2d = new Texture2D(Screen.width, Screen.height, TextureFormat.RGBA32, false);
                 tex2d.ReadPixels(new Rect(0,0, Screen.width, Screen.height), 0 ,0, false);
                 tex2d.Apply();
+                OutputTextureToDisk(tex2d);
                 break;
-            case ImageExportMethod.GameViewRT:
+            case ImageExportMethod.GameViewRenderTexture:
                 ScreenCapture.CaptureScreenshotIntoRenderTexture(captureTexture);
-                tex2d = new Texture2D(Screen.width, Screen.height, TextureFormat.RGBA32, false);
-                // ReadPixels looks at the active RenderTexture.
-                var previous = RenderTexture.active;
-                RenderTexture.active = captureTexture;
-                tex2d.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
-                tex2d.Apply();
-                RenderTexture.active = previous; // restore previous
+                AsyncGPUReadback.Request(captureTexture, 0, TextureFormat.RGBA32, ReadbackCompleted);
                 break;
             default:
                 throw new InvalidEnumArgumentException($"Unexpected image export method '{method}'");
         }
-        var imgBytes = tex2d.EncodeToPNG();
-        File.WriteAllBytes(currentFile.FullName, imgBytes);
-        Debug.Log($"Wrote frame #{Time.frameCount} to disk as {currentFile.FullName}");
+    }
+
+    private void ReadbackCompleted(AsyncGPUReadbackRequest obj)
+    {
+        // Render texture no longer needed, it has been read back.
+        DestroyImmediate(captureTexture);
+
+        using (var imgBytes = obj.GetData<byte>())
+        {
+            var tex2d = new Texture2D(Screen.width, Screen.height, TextureFormat.RGBA32, false);
+            tex2d.LoadRawTextureData(imgBytes);
+            OutputTextureToDisk(tex2d);
+        }
+
+        CreateRenderTexture();
     }
 }
