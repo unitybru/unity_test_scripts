b86205415a56134ceac1917062de940d7b13e926
Bruno Angeles <bruno.angeles@unity3d.com>
2022-05-25 12:22:25 -0400

    Add logic path for GameView capture

--- ImageExporter.cs
+++ ImageExporter.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 public class ImageExporter : MonoBehaviour
 {
     public KeyCode recordKey = KeyCode.Space;
-
+    public bool recordGameView = true; // whether or not to capture from the GameView
 
     // Update is called once per frame
     void Update()
@@ -24,16 +24,22 @@ public class ImageExporter : MonoBehaviour
         // Wait until after the frame has been prepared
         yield return new WaitForEndOfFrame();
 
-        var paddedFrameString = Time.frameCount.ToString().PadLeft(4, '0');
+        var paddedFrameString = Time.frameCount.ToString().PadLeft(8, '0');
         var currentFile = new FileInfo($"test_{paddedFrameString}.png");
 
-        // Add video frame
-        Texture2D texture = new Texture2D(Screen.width , Screen.height , TextureFormat.RGBA32 , false);
-        texture.ReadPixels(new Rect(0,0,Screen.width, Screen.height), 0 ,0, false);
-        texture.Apply();
-        var imgBytes = texture.EncodeToPNG();
+        Texture2D tex2d;
+        if (recordGameView)
+        {
+            tex2d = ScreenCapture.CaptureScreenshotAsTexture();
+        }
+        else
+        {
+            tex2d = new Texture2D(Screen.width , Screen.height , TextureFormat.RGBA32 , false);
+            tex2d.ReadPixels(new Rect(0,0,Screen.width, Screen.height), 0 ,0, false);
+            tex2d.Apply();
+        }
+        var imgBytes = tex2d.EncodeToPNG();
         File.WriteAllBytes(currentFile.FullName, imgBytes);
         Debug.Log($"Wrote frame #{Time.frameCount} to disk as {currentFile.FullName}");
-
     }
 }
